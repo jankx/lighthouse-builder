5f0f96686706d4284434011887e12a0347294285
Puleeno Nguyen <puleeno@gmail.com>
2020-12-25 10:43:51 +0700

    Init plugins

---
+++ lighthouse-builder.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Lighthouse Builder
+ * Plugin URI: https://github.com/jankx/lighthouse-builder/
+ * Description:  The WordPress page builder for Jankx framework follow Google Lighthouse to Improve Pagespeed Insights
+ * Version: 0.0.1
+ * Author: Puleeno Nguyen
+ * Author URI: https://puleeno.com
+ * Tag: Page builder, pagespeed
+ */
